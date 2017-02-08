474b4ad1c619c6966b94384972c60a9a77e52ae4
Thimy Khotim <khotim@users.noreply.github.com>
2017-02-08 21:05:13 +0700

    Update DatetimePicker.php

--- DatetimePicker.php
+++ DatetimePicker.php
@@ -44,10 +44,13 @@ class DatetimePicker extends \yii\jui\DatePicker
      */
     public $options = [];
     /**
-     * @var array the HTML attributes for the picker button.
-     * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
+     * @var boolean If set to true, it will render as bootstrap input group.
      */
-    public $buttons = [];
+    public $inputGroup = true;
+    /**
+     * @var string the CSS for the picker button. Used only when $inputGroup is set to true.
+     */
+    public $buttonCssClass = 'btn btn-default';
 
     /**
      * Renders the widget.
@@ -58,13 +61,19 @@ class DatetimePicker extends \yii\jui\DatePicker
 
         $containerID = $this->inline ? $this->containerOptions['id'] : $this->options['id'];
         $language = $this->language ? $this->language : Yii::$app->language;
+        $inputGroupJs = null;
         
         if ($this->dateOnly) {
             $this->pickerID = "datepicker";
         } elseif ($this->timeOnly) {
             $this->pickerID = "timepicker";
         }
-
+        
+        if ($this->inputGroup) {
+            $this->clientOptions['showOn'] = 'button';
+            $this->clientOptions['buttonText'] = '<span class="glyphicon glyphicon-calendar"></span>';
+        }
+        
         if ($language !== 'en-US') {
             $view = $this->getView();
             $assetBundle = LanguageAsset::register($view);
@@ -82,22 +91,64 @@ class DatetimePicker extends \yii\jui\DatePicker
             }
             
             $view->registerJs("$('#{$containerID}').{$this->pickerID}($.extend({}, {$localizeDate}, {$localizeTime}, $options))");
-            
-            if (isset($this->clientOptions['showOn'])) {
-                if (isset($this->buttons['class']))
-                    $buttonCssClass = $this->buttons['class'];
-                else
-                    $buttonCssClass = 'btn btn-default';
-                
-                $view->registerJs("$('#{$containerID}').next('.ui-datepicker-trigger')
-                    .addClass({$buttonCssClass})
-                    .wrap({$this->buttonText});");
-            }
         } else {
             $this->registerClientOptions($this->pickerID, $containerID);
         }
-
+        
+        if ($this->inputGroup) {
+            $this->getView()->registerJs("jQuery('#{$containerID}').next('.ui-datepicker-trigger').addClass('{$this->buttonCssClass}').wrap('<span class=\"input-group-btn\">');");
+        }
         $this->registerClientEvents($this->pickerID, $containerID);
         BaseAsset::register($this->getView());
+    }
+    
+    /**
+     * Renders the DatePicker widget.
+     * @return string the rendering result.
+     */
+    protected function renderWidget()
+    {
+        $contents = [];
+
+        // get formatted date value
+        if ($this->hasModel()) {
+            $value = Html::getAttributeValue($this->model, $this->attribute);
+        } else {
+            $value = $this->value;
+        }
+        if ($value !== null && $value !== '') {
+            // format value according to dateFormat
+            try {
+                $value = Yii::$app->formatter->asDate($value, $this->dateFormat);
+            } catch(InvalidParamException $e) {
+                // ignore exception and keep original value if it is not a valid date
+            }
+        }
+        $options = $this->options;
+        $options['value'] = $value;
+
+        if ($this->inline === false) {
+            if ($this->hasModel()) {
+                $content = Html::activeTextInput($this->model, $this->attribute, $options);
+            } else {
+                $content = Html::textInput($this->name, $value, $options);
+            }
+            
+            // render a text input
+            $contents[] = $this->inputGroup ? '<div class="input-group">'.$content.'</div>' : $content;
+            
+        } else {
+            // render an inline date picker with hidden input
+            if ($this->hasModel()) {
+                $contents[] = Html::activeHiddenInput($this->model, $this->attribute, $options);
+            } else {
+                $contents[] = Html::hiddenInput($this->name, $value, $options);
+            }
+            $this->clientOptions['defaultDate'] = $value;
+            $this->clientOptions['altField'] = '#' . $this->options['id'];
+            $contents[] = Html::tag('div', null, $this->containerOptions);
+        }
+
+        return implode("\n", $contents);
     }
 }
